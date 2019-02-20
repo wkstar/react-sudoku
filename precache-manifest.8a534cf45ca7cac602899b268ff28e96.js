09a05afe52d73474efe9952511f3c54f66cb8464
Tom Mead <530462+wkstar@users.noreply.github.com>
2019-02-20 12:37:07 +0000

    Updates

---
+++ precache-manifest.8a534cf45ca7cac602899b268ff28e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe632665c8d52e28530",
+    "url": "/react-sudoku/static/css/main.2bb9872c.chunk.css"
+  },
+  {
+    "revision": "3fe632665c8d52e28530",
+    "url": "/react-sudoku/static/js/main.3fe63266.chunk.js"
+  },
+  {
+    "revision": "65db77262c9d0efae00e",
+    "url": "/react-sudoku/static/js/1.65db7726.chunk.js"
+  },
+  {
+    "revision": "41684abc48e4fa4f991f",
+    "url": "/react-sudoku/static/js/runtime~main.41684abc.js"
+  },
+  {
+    "revision": "93fae9e5c307eef38163b977d07e95d7",
+    "url": "/react-sudoku/index.html"
+  }
+];
